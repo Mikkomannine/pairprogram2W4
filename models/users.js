6e9e956169ed967207e4f47108607981e2b5914d
Mikko <mikko.manninen3@metropolia.fi>
2024-02-07 18:52:46 +0200

    push3

--- models/users.js
+++ models/users.js
@@ -1,34 +1,22 @@
-const users = [
-    {
-        id: 1,
-        username: "adminUser",
-        email: "admin@example.com",
-        role: "admin",
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
     },
-    {
-        id: 2,
-        username: "guestUser",
-        email: "guest@example.com",
-        role: "guest",
+    name: {
+        type: String,
+        required: true,
     },
-    {
-        id: 3,
-        username: "agencyUser",
-        email: "agency@example.com",
-        role: "travel agency",
+    email: {
+        type: String,
+        required: true,
     },
-    {
-        id: 4,
-        username: "user1",
-        email: "user1@example.com",
-        role: "guest",
+    role: {
+        type: String,
+        required: true,
     },
-    {
-        id: 5,
-        username: "user2",
-        email: "user2@example.com",
-        role: "guest",
-    },
-];
+});
+const User = mongoose.model('User', userSchema);
 
-module.exports = users;
+module.exports = User;
